ace5593b6ddd48ec657eef0e08e6006085503223
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-02-13 16:48:17 +0330

    Create my.lua

---
+++ plugins/my.lua
@@ -0,0 +1,13 @@
+do
+ local function run(msg)
+ if msg.text == "myuser" then 
+ return "msg.from.id"
+ end
+ if msg.text == "myfirst" then 
+ return "msg.from.id"
+ end
+ description = { my pattern
+ }
+ patterns = {
+"^([Mm]yuser)$"
+"^[[Mm]yfirst)$"
