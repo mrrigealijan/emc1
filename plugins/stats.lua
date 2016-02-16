b1d1163bcb9be152d67a8412b96f73c28e0411d7
rigeirani <ccaahh430@gmail.com>
2016-02-16 14:20:07 +0000

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'megasatan' then -- Put everything you like :)
+  if matches[1]:lower() == 'msf' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /idetergent ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "megasatan" then -- Put everything you like :)
+    if matches[2] == "msf" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -148,8 +148,8 @@ return {
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (megasatan)",-- Put everything you like :)
-		"^([Mm]egasatan)"-- Put everything you like :)
+    "^([Ss]tats) (msf)",-- Put everything you like :)
+		"^([Mm]sf)"-- Put everything you like :)
     }, 
   run = run
 }
